68cb12e3aa9ca0d333a20f3e7d96549a4cb45825
DiviCon <desarrolloefectivo5@gmail.com>
2023-06-01 17:46:56 -0500

    DEV - Creacion inicial del archivo config.php

--- app/config/bee_config.php
+++ app/config/bee_config.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 // Saber si trabajamos en local o remoto
 define('IS_LOCAL', in_array($_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::']));
@@ -12,9 +12,39 @@ define('LANG', 'es');
 // Ruta base del proyecto
 define('BASEPATH', IS_LOCAL ? '/beeframework/' : '___EL BASEPATH EN PRODUCCION___');
 
+// Generar una cadena aleatoria para la sal del sistema
+$randomBytes = random_bytes(32);
+$salt = bin2hex($randomBytes);
+
 // Sal del sistema
-define('AUTH_SALT', 'BeeFramework<3');
+define('AUTH_SALT', $salt);
+
 
 // Puerto y la url del sitio
 define('PORT', '8848');
-define('URL', IS_LOCAL ? 'http://127.0.0.1:'.PORT.'/beeframework/' : '___URL EN PRODUCCION___');
+define('URL', IS_LOCAL ? "http://127.0.0.1:".PORT.BASEPATH : '___URL EN PRODUCCION___');
+
+// Rutas de nuestro directorio y archivos
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT', __DIR__.DS);
+
+define('APP', ROOT.'app'.DS);
+define('CLASSES', APP.'classes'.DS);
+define('CONFIG', APP.'config'.DS);
+define('CONTROLLERS', APP.'controllers'.DS);
+define('FUNCTIONS', APP.'functions'.DS);
+define('MODELS', APP.'models'.DS);
+
+define('TEMPLATES', ROOT.'templates'.DS);
+define('INCLUDES', TEMPLATES.'includes'.DS);
+define('MODULES', TEMPLATES.'modules'.DS);
+define('VIEWS', TEMPLATES.'views'.DS);
+
+define('ASSETS', URL.'assets/');
+define('CSS', URL.'css/');
+define('FAVICON', URL.'favicon/');
+define('FONTS', URL.'fonts/');
+define('IMAGES', URL.'images/');
+define('JS', URL.'js/');
+define('PLUGINS', URL.'plugins/');
+define('UPLOADS', URL.'uploads/');
